41069536f355ab74b3b0d29df61e0b45ad8dc915
KoheiAsano <komerumeru@i.softbank.jp>
2018-12-20 16:54:10 +0900

    int fuck

--- problems/C/038.cpp
+++ problems/C/038.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+#define ll long long
 
 
 int main(){
-	int N; cin >> N;
-	int a[N];
-	int ans =0;
+	ll N; cin >> N;
+	ll a[N];
+	ll ans =0;
 	for(int i=0;i<N;i++){
 		cin >> a[i];
 		ans ++;
 	}
 
 	for(int i=0;i<N;i++){
-		int p = a[i];
-    int seqcount = 0;
+		ll p = a[i];
+    ll seqcount = 0;
 		for(int j=i+1;j<N;j++){
 			if(p<a[j]){
         seqcount++;
-				cout << "(" << i << j << ")" << endl;
+				// cout << "(" << i << j << ")" << endl;
 				ans+=seqcount;
 				p = a[j];
 			}
