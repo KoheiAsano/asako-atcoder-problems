1556762d39de5df08d38df3ab65de687e3c9801b
KoheiAsano <komerumeru@i.softbank.jp>
2018-12-20 17:24:52 +0900

    stupid miss

--- problems/C/039.py
+++ problems/C/039.py
@@ -1,29 +1,18 @@
-# PIANO = "WBWBWWBWBWBW"
-# MAP = {0:"Do",1:"Do", 2:"Re",
-#        3:"Re", 4:"Mi", 5:"Fa",
-#        6:"Fa", 7:"So", 8:"So",
-#        9:"La", 10:"La", 11:"Si"}
-
-ANS = {"WBWBWWBWBWBWWBWBWWBW":"Do",
-       "WBWWBWBWBWWBWBWWBWWB":"Re",
-"WWBWBWBWWBWBWWBWWBWB":"Mi",
-"WBWBWBWWBWBWWBWWBWBW":"Fa",
-"WBWBWWBWBWWBWWBWBWWB":"So",
-"WBWWBWBWWBWWBWBWWBWB":"La",
-"WWBWBWWBWWBWBWWBWBWB":"Si"}
-
+# PIANO = "WBWBWWBWBWBWWBWBWWBWBWBWWBWBWWBWBWBW"
+# for i in range(12):
+#     print(PIANO[i:i+20])
 S = str(input())
-print(ANS[S])
-#
-# for i in range(len(S)):
-#     if(S[i:i+12] == PIANO):
-#         print(MAP[(12-i)%12])
-
-# print()
-# for i in range(20 -1):
-#     # print(MAP[i+1])
-#     print(Do[(i+1)%12:] + PIANO[:(i+1)%12] + ":" +MAP[(i+1)%12])
-# # Do
-# # WBWBWWBWBWBWWBWBWWBW
-# # Do
-# # BWBWWBWBWBW:WBWBWWBWB
+if(S == "WBWBWWBWBWBWWBWBWWBW"):
+    print("Do")
+elif(S == "WBWWBWBWBWWBWBWWBWBW"):
+    print("Re")
+elif(S == "WWBWBWBWWBWBWWBWBWBW"):
+    print("Mi")
+elif(S == "WBWBWBWWBWBWWBWBWBWW"):
+    print("Fa")
+elif(S == "WBWBWWBWBWWBWBWBWWBW"):
+    print("So")
+elif(S == "WBWWBWBWWBWBWBWWBWBW"):
+    print("La")
+elif(S == "WWBWBWWBWBWBWWBWBWWB"):
+    print("Si")
