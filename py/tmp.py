0ca4844fb9001e40fedaaa0690a2a79c59b19218
KoheiAsano <komerumeru@i.softbank.jp>
2019-01-22 10:29:12 +0900

    update ttmp

--- py/tmp.py
+++ py/tmp.py
@@ -1,13 +1,8 @@
 import sys
 # A,B,C = [int(n) for n in input().split()]
 # N = int(input())
+# A = [0]*N
+# for i in range(N):
+#     A[i] = int(input())
 # S = str(input())
 # T = str(input())
-a,b,c = [int(n) for n in input().split()]
-
-if(b <= c and a <= c):
-    print(10*c + a + b)
-elif(a <= b and c <= b):
-    print(10*b + c + a)
-elif(c <= a and b <= a):
-    print(10*a + c + b)
