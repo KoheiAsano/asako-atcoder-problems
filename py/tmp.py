696887d43716b20847109502364a4fa7ecbceaff
KoheiAsano <komerumeru@i.softbank.jp>
2019-02-26 17:41:40 +0900

    add oneline input by python

--- py/tmp.py
+++ py/tmp.py
@@ -1,8 +1,6 @@
 import sys
 # A,B,C = [int(n) for n in input().split()]
 # N = int(input())
-# A = [0]*N
-# for i in range(N):
-#     A[i] = int(input())
+# a = [int(input()) for _ in range(N)]
 # S = str(input())
 # T = str(input())
