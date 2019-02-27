dc36d0a750c9687186233dfb5783e7da85ccf83c
KoheiAsano <komerumeru@i.softbank.jp>
2019-02-27 16:54:38 +0900

    ABC

---
+++ problems/A/019.py
@@ -0,0 +1,8 @@
+import sys
+ABC = [int(n) for n in input().split()]
+# N = int(input())
+# a = [int(input()) for _ in range(N)]
+# S = str(input())
+# T = str(input())
+ABC = sorted(ABC)
+print(ABC[1])
