7c2472cd6a2bdfa57879174acfed40eab8ee6fa1
KoheiAsano <komerumeru@i.softbank.jp>
2019-03-04 14:16:45 +0900

    120AC

---
+++ problems/A/120.py
@@ -0,0 +1,12 @@
+import sys
+A,B,C = [int(n) for n in input().split()]
+# N = int(input())
+# a = [int(input()) for _ in range(N)]
+# S = str(input())
+# L = len(S)
+# T = str(input())
+taka = 0
+while (taka != C and B >= A):
+    B-=A
+    taka+=1
+print(taka)
