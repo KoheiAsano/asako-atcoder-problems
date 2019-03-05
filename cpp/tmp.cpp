aaf3568350f4a06bb9fa7f978ddca4bf01bac22a
KoheiAsano <komerumeru@i.softbank.jp>
2019-03-05 11:11:50 +0900

    add input to cpp

--- cpp/tmp.cpp
+++ cpp/tmp.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int N,K;
 
 int main(){
+	cin >> N >> K;
+	int A[N];
+	for(int i=0;i<N;i++){
+		cin >> A[i];
+	}
 	cout << "Hello, world" << endl;
 }
