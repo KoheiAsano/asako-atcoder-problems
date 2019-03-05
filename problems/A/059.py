01124b5deca2808ab4478bae2cb24ee8a3938c2d
KoheiAsano <komerumeru@i.softbank.jp>
2019-03-05 12:37:04 +0900

    A

---
+++ problems/A/059.py
@@ -0,0 +1,8 @@
+import sys
+A,B,C = [str(n) for n in input().split()]
+# N = int(input())
+# a = [int(input()) for _ in range(N)]
+# S = str(input())
+# L = len(S)
+# T = str(input())
+print(A[0].upper() + B[0].upper() + C[0].upper())
