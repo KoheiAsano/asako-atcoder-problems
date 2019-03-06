f2b9fefcb98109953a79ac801cb62f9ea4aba662
KoheiAsano <komerumeru@i.softbank.jp>
2019-03-06 16:57:47 +0900

    AC ABC

---
+++ problems/A/042.py
@@ -0,0 +1,10 @@
+ABC = sorted([int(n) for n in input().split()])
+# N = int(input())
+# a = [int(input()) for _ in range(N)]
+# S = str(input())
+# L = len(S)
+# T = str(input())
+if ABC[0] == 5 and ABC[1] == 5 and ABC[2] == 7:
+    print("YES")
+else:
+    print("NO")
