4b8e54c3c4261f1a02726fc4ad3d9fee95bdda32
KoheiAsano <komerumeru@i.softbank.jp>
2019-03-18 14:31:04 +0900

    tweak 2-dimension sort

--- py/utils/sort_2d.py
+++ py/utils/sort_2d.py
@@ -1,6 +1,6 @@
 N=int(input())
-red=sorted([list(map(int,input().split())) for i in range(N)], key=lambda x:-1*x[1])# y座標でSort(Decrease)
-blue=sorted([list(map(int,input().split())) for i in range(N)])#x座標Increse
+red=sorted([[int(n) for n in input().split()] for i in range(N)], key=lambda x:-1*x[1])# y座標でSort(Decrease)
+blue=sorted([int(n) for n in input().split()] for i in range(N)])#x座標Increse
 count=0
 for i in red:
     for n,j in enumerate(blue):
