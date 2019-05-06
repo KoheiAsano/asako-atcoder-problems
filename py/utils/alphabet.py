4d5cf6fb5c529c0170a6578fa386e90a2889e3d8
KoheiAsano <komerumeru@i.softbank.jp>
2019-05-06 14:24:39 +0900

    update

--- py/utils/alphabet.py
+++ py/utils/alphabet.py
@@ -1,7 +1,8 @@
-alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n',
+         'o', 'p', 'q', 'r', 's', 't', 'u',
+          'v', 'w', 'x', 'y', 'z']
 alp = [a.upper() for a in alp]
-index = [n for n in range(1,27)]
+index = [n for n in range(1, 27)]
 print(alp)
 print(dict(zip(alp, index)))
-
-
