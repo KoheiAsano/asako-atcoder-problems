0b7dc68ccdf8c1f59b1b2120c234e9c9342f10a7
KoheiAsano <komerumeru@i.softbank.jp>
2019-05-16 20:39:49 +0900

    pyliint

--- py/utils/uf_size.py
+++ py/utils/uf_size.py
@@ -1,12 +1,13 @@
 class UnionFind():
-    #負の値は根で集合の個数
-    #正の値は次の要素を返す
-    def __init__(self,size):
-        self.table = [-1 for _  in range(size)]
+    # 負の値は根で集合の個数
+    # 正の値は次の要素を返す
+    def __init__(self, size):
+        self.table = [-1 for _ in range(size)]
 
-    #集合の代表を求める
-    def find(self,x):
-        if self.table[x] < 0:return x
+    # 集合の代表を求める
+    def find(self, x):
+        if self.table[x] < 0:
+            return x
         root = self.find(self.table[x])
         self.table[x] = root
         return root
@@ -15,25 +16,27 @@ class UnionFind():
         #     x = self.table[x]
         # return x
 
-    #併合
-    def union(self,x,y):
-        s1 = self.find(x)#代表のIndexを取得する
+    # 併合
+    def union(self, x, y):
+        s1 = self.find(x)  # 代表のIndexを取得する
         s2 = self.find(y)
-        if s1 != s2:#代表のIndexが違う場合
-            if self.table[s1] < self.table[s2]:#要素数の多い方へMergeする
-                self.table[s2] += self.table[s1]#子の和を増やす(-)
-                self.table[s1] = s2#Mergeされる側の代表をする側のインデックスに
+        if s1 != s2:  # 代表のIndexが違う場合
+            if self.table[s1] < self.table[s2]:  # 要素数の多い方へMergeする
+                self.table[s2] += self.table[s1]  # 子の和を増やす(-)
+                self.table[s1] = s2  # Mergeされる側の代表をする側のインデックスに
             else:
                 self.table[s1] += self.table[s2]
                 self.table[s2] = s1
         return
-    def size(self,x):
+    def size(self, x):
         # while self.table[x] >= 0:
         #     #根に来た時,self.table[根のindex]は負の値なのでx = 根のindexで値が返される。
         #     x = self.table[x]
         return -self.table[self.find(x)]
+
+
 UF = UnionFind(9)
-UF.union(0,1)
+UF.union(0, 1)
 print(UF.table)
 print(UF.size(1))
 print(UF.size(0))
