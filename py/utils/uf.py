8f2f584c033c78d813ab2bc8d299b0882839f0ab
KoheiAsano <komerumeru@i.softbank.jp>
2019-05-27 17:52:27 +0900

    no-bug uf

--- py/utils/uf.py
+++ py/utils/uf.py
@@ -1,32 +1,30 @@
-class UnionFind():
-    #負の値はルートで集合の個数
-    #正の値は次の要素を返す
-    def __init__(self,size):
-        self.table = [-1 for _  in range(size)]
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n+1)]
+        self.rank = [0] * (n+1)
 
-    #集合の代表を求める
-    def find(self,x):
-        while self.table[x] >= 0:
-            #根に来た時,self.table[根のindex]は負の値なのでx = 根のindexで値が返される。
-            x = self.table[x]
-        return x
+    # 検索
+    def find(self, x):
+        if self.par[x] == x:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
 
-    #併合
-    def union(self,x,y):
-        s1 = self.find(x)#根のindex,table[s1]がグラフの高さ
-        s2 = self.find(y)
-        if s1 != s2:#根が異なる場合
-            if self.table[s1] != self.table[s2]:#グラフの高さが異なる場合
-                if self.table[s1] < self.table[s2]:
-                    self.table[s2] = s1
-                else:
-                    self.table[s1] = s2
-            else:
-                #グラフの長さが同じ場合,どちらを根にしても変わらない
-                #その際,グラフが1長くなることを考慮する
-                self.table[s1] += -1
-                self.table[s2] = s1
-        return
+    # 併合
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if self.rank[x] < self.rank[y]:
+            self.par[x] = y
+        else:
+            self.par[y] = x
+            if self.rank[x] == self.rank[y]:
+                self.rank[x] += 1
+
+    # 同じ集合に属するか判定
+    def same_check(self, x, y):
+        return self.find(x) == self.find(y)
 UF = UnionFind(9)
 print(UF)
 UF.union(0,1)
