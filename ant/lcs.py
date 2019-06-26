13f2bc6a229c835a65c75c9f3ad5838356f8ed2b
KoheiAsano <kk.asano.luxy@gmail.com>
2019-06-26 17:26:11 +0900

    longest common subsequence

--- ant/lcs.py
+++ ant/lcs.py
@@ -9,6 +9,6 @@ for s_cur in range(n):
     for t_cur in range(m):
         c = 1 if s[s_cur] == t[t_cur] else 0
         memo[s_cur+1][t_cur+1] = max(memo[s_cur][t_cur+1], memo[s_cur+1][t_cur], memo[s_cur+1][t_cur] + c)
-for i in range(n+1):
-    print(memo[i])
+# for i in range(n+1):
+#     print(memo[i])
 memo[n][m]
