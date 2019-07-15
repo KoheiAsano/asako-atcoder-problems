e997f5b9e54d175b30e0812c36260b913da4f9b4
KoheiAsano <kk.asano.luxy@gmail.com>
2019-07-15 17:11:41 +0900

    implement debug function

--- py/tmp.py
+++ py/tmp.py
@@ -1,4 +1,9 @@
-# from sys import exit
+# from sys import exit, stderr
+# from inspect import currentframe
+# def debug(*args):
+#     names = {id(v):k for k,v in currentframe().f_back.f_locals.items()}
+#     print(', '.join(names.get(id(arg),'???')+' = '+repr(arg) for arg in args), file=stderr)
+#     return
 # A, B, C = [int(n) for n in input().split()]
 # N = int(input())
 # a = [int(input()) for _ in range(N)]
