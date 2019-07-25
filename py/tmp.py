ebda076160d5b9cd523d4528d0d776394912e5f3
KoheiAsano <kk.asano.luxy@gmail.com>
2019-07-25 12:17:37 +0900

    update debug function

--- py/tmp.py
+++ py/tmp.py
@@ -1,8 +1,7 @@
-# from sys import exit, stderr
-# from inspect import currentframe
-# def debug(var, name="hoge"):
-#     print(name + " = " + repr(var), file=stderr)
-#     return
+from sys import exit, stderr
+def debug(var, name="hoge"):
+    print(name + str(id(var)) +" = " + repr(var), file=stderr)
+    return
 # A, B, C = [int(n) for n in input().split()]
 # N = int(input())
 # a = [int(input()) for _ in range(N)]
