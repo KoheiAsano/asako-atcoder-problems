4ce0d607324e758442dabf823dee54d8a6dc7d90
KoheiAsano <kk.asano.luxy@gmail.com>
2019-08-11 21:34:01 +0900

    stdinput readline

--- py/tmp.py
+++ py/tmp.py
@@ -1,14 +1,17 @@
-from sys import exit, stderr
+from sys import exit, stderr, stdin
+input = stdin.readline
 def debug(var, name="hoge"):
     print(name +":" + str(type(var)) + " = " + repr(var), file=stderr)
     return
 
 
 def main():
-    # A, B, C = [int(n) for n in input().split()]
+    A, B, C = [int(n) for n in input().split()]
+    print(A,B,C)
     # N = int(input())
     # a = [int(input()) for _ in range(N)]
-    # S = str(input())
+    # S = str(input())[:-1]
+    # print(S)
     # L = len(S)
     # INF = 10000000002
     # EPS = 1e-9
