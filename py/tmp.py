da3c498e93890ab60062daf24e809f76335a3a2b
KoheiAsano <kk.asano.luxy@gmail.com>
2019-08-11 21:25:27 +0900

    change template

--- py/tmp.py
+++ py/tmp.py
@@ -2,11 +2,19 @@ from sys import exit, stderr
 def debug(var, name="hoge"):
     print(name +":" + str(type(var)) + " = " + repr(var), file=stderr)
     return
-# A, B, C = [int(n) for n in input().split()]
-# N = int(input())
-# a = [int(input()) for _ in range(N)]
-# S = str(input())
-# L = len(S)
-# INF = 10000000002
-# EPS = 1e-9
-# exit()
+
+
+def main():
+    # A, B, C = [int(n) for n in input().split()]
+    # N = int(input())
+    # a = [int(input()) for _ in range(N)]
+    # S = str(input())
+    # L = len(S)
+    # INF = 10000000002
+    # EPS = 1e-9
+    # exit()
+
+
+
+if __name__ == "__main__":
+    main()
